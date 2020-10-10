250a2c300074fdda6acc914a9b29436aab7a1ee1
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-10-10 16:35:56 +0200

    File structure / collision and more enemies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -9,31 +10,37 @@ HEIGHT = 600
 GAME_SPEED = .5
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load('img/icon.png')
 pygame.display.set_icon(icon)
-backGround = pygame.image.load('bg.jpg')
+backGround = pygame.image.load('img/bg.jpg')
 backGround = pygame.transform.scale(backGround, (WIDTH, HEIGHT))
 
 # PLAYER
-playerImg = pygame.image.load('player1.png')
+playerImg = pygame.image.load('img/player1.png')
 playerImg = pygame.transform.scale(playerImg, (60, 60))
 playerX = WIDTH / 2 - 30
 playerY = HEIGHT - 80
 playerX_change_left = 0
 playerX_change_right = 0
+score = 0
 
 
 def player(x):
     screen.blit(playerImg, (x, playerY))
 
 
-# ENEMY
-enemyImg = pygame.image.load('icon.png')
-enemyImg = pygame.transform.scale(enemyImg, (60, 60))
-enemyX = random.randint(20, WIDTH - 80)
-enemyY = 20
+# ENEMIES
+enemyX = []
+enemyY = []
+enemy_amount = 6
 enemyX_change = 2 * GAME_SPEED
 enemyY_change = 30
+enemyImg = pygame.image.load('img/icon.png')
+enemyImg = pygame.transform.scale(enemyImg, (60, 60))
+
+for i in range(enemy_amount):
+    enemyX.append(random.randint(60, WIDTH - 120))
+    enemyY.append(20)
 
 
 def enemy(x, y):
@@ -41,7 +48,7 @@ def enemy(x, y):
 
 
 # PROJECTILE
-projectileImg = pygame.image.load('blue_laser.png')
+projectileImg = pygame.image.load('img/blue_laser.png')
 projectileImg = pygame.transform.scale(projectileImg, (20, 50))
 projectileX = 0
 projectileY = HEIGHT - 80
@@ -53,6 +60,14 @@ def fire_projectile(x, y):
     global projectile_state
     projectile_state = "fire"
     screen.blit(projectileImg, (x + 20, y + 20))
+
+
+def collision(enemyX, enemyY, projectileX, projectileY):
+    distance = math.sqrt(math.pow(enemyX - projectileX, 2) + math.pow(enemyY - projectileY, 2))
+    if distance < 25:
+        return True
+    else:
+        return False
 
 
 # GAME
@@ -87,13 +102,24 @@ while running:
         playerX = WIDTH - 80
 
     # ENEMY LOGIC
-    enemyX += enemyX_change
-    if enemyX < 20:
-        enemyX_change = 2 * GAME_SPEED
-        enemyY += enemyY_change
-    elif enemyX > WIDTH - 80:
-        enemyX_change = -2 * GAME_SPEED
-        enemyY += enemyY_change
+    for i in range(enemy_amount):
+        enemyX[i] += enemyX_change
+        if enemyX[i] < 20:
+            enemyX_change = 2 * GAME_SPEED
+            for j in range(enemy_amount):
+                enemyY[j] += enemyY_change
+        elif enemyX[i] > WIDTH - 80:
+            enemyX_change = -2 * GAME_SPEED
+            for j in range(enemy_amount):
+                enemyY[j] += enemyY_change
+        hit = collision(enemyX[i], enemyY[i], projectileX, projectileY)
+        if hit:
+            projectileY = HEIGHT - 80
+            projectile_state = "ready"
+            score += 1
+            enemyX[i] = random.randint(60, WIDTH - 120)
+            enemyY[i] = 20
+        enemy(enemyX[i], enemyY[i])
 
     # PROJECTILE LOGIC
     if projectile_state == "fire":
@@ -104,5 +130,4 @@ while running:
         projectile_state = "ready"
 
     player(playerX)
-    enemy(enemyX, enemyY)
     pygame.display.update()
